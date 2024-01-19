5e8895e080bc2461e881335fd42362e1e9364696
ulikemyway1 <calium@outlook.com>
2024-01-20 01:39:34 +0300

    fix: 04 implement nested copying and fix update problem

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,15 +1,31 @@
 const fsP = require('fs/promises');
 const path = require('path');
 
-fsP.mkdir(path.resolve(__dirname, 'files-copy'), { recursive: true }).then(
-  fsP
-    .readdir(path.resolve(__dirname, 'files'), { withFileTypes: true })
-    .then((obj) => {
-      obj.forEach((item) =>
-        fsP.copyFile(
-          path.resolve(item.path, item.name),
-          path.resolve(__dirname, 'files-copy', item.name),
-        ),
-      );
+const inputDirectory = path.resolve(__dirname, 'files-copy');
+const outputDirectory = path.resolve(__dirname, 'files');
+
+function copy(src, dist) {
+  fsP.mkdir(dist, { recursive: true }).then(
+    fsP.readdir(src, { withFileTypes: true }).then((obj) => {
+      obj.forEach((item) => {
+        if (item.isDirectory()) {
+          const newSrc = path.resolve(item.path, item.name);
+          const newDist = newSrc.replace('files', 'files-copy');
+          copy(newSrc, newDist);
+        } else {
+          fsP.copyFile(
+            path.resolve(item.path, item.name),
+            path.resolve(dist, item.name),
+          );
+        }
+      });
     }),
-);
+  );
+}
+
+async function run() {
+  await fsP.rm(inputDirectory, { recursive: true, force: true });
+  copy(outputDirectory, inputDirectory);
+}
+
+run();
